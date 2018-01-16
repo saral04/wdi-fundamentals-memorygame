493cc8269035ed43d2b3736ed6ec90e638fd742d
Sara Boncha <sara.boncha@disney.com>
2018-01-16 00:41:59 -0800

    created game logic with Reset Game button

--- js/main.js
+++ js/main.js
@@ -23,19 +23,41 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+  var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
-  checkForMatch();
+  this.setAttribute("src", cards[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  } else {
+    null;
+  }
 };
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+
+createBoard();
+
+var resetGame = function() {
+    location.reload();
+};
+
+document.getElementsByTagName("button")[0].addEventListener("click", resetGame);
